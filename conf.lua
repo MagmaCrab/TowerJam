c5de00e03805520b6450b7a402ca5d575e3cef8c
Hidde van Bavel <hiddevanbavel@hotmail.com>
2016-02-03 22:01:01 +0100

    Test file edits

--- conf.lua
+++ conf.lua
@@ -1,9 +1,18 @@
+--[[
+	standard configuration file with PAL screen size
+]]--
+
 function love.conf(t)
-    t.window.title = "Tower jam"        -- The title of the window the game is in (string)
-	t.window.width = 768 --384
-    t.window.height = 576 --288
-    t.console = true           -- Attach a console (boolean, Windows only)
-    t.release = false           -- Enable release mode (boolean)
-    t.window.vsync = false       -- Enable vertical sync (boolean)
-    t.window.fsaa = 0           -- The number of FSAA-buffers (number)
+	t.screen = t.screen or t.window
+	
+	t.title = "Tower Jam"
+	
+	t.screen.width = 768        
+	t.screen.height = 576       
+	t.screen.borderless = false  
+	t.screen.resizable = false  
+	t.screen.fullscreen = false 
+	t.screen.vsync = false       
+	t.screen.fsaa = 0   
+	--t.screen.icon = 0
 end
