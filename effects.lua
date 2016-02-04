1e381af1b5f142236eb63d71cfc3c96fd86401d2
Casper van Bavel <caspervanbavel@gmail.com>
2016-02-05 00:15:31 +0100

    fixed git fuckup

--- effects.lua
+++ effects.lua
@@ -7,10 +7,12 @@ function effects:create()
 	self.animation = Animation:create(self.dustImg, 10)
 	self.x = 0
 	self.y = 0
+	self.clouds = Cloud:create()
 end
 
 function effects:update(dt)
 	self.animation:update(dt)
+	self.clouds:update(dt)
 end
 
 function effects:draw()
@@ -19,11 +21,33 @@ function effects:draw()
 		self.animation:draw(math.floor(self.x)-8, math.floor(self.y)-8)
 	end
 	--draw clouds
-
+	love.graphics.setColor(255, 255, 255)
+	self.clouds:draw()
 end
 
 function effects:add(x,y)
 	self.x = x
 	self.y = y 
 	self.animation:reset()
+end
+
+Cloud = {}
+
+function Cloud:create()
+	local new = {}
+	setmetatable(new, self)
+	self.__index = self
+	
+	new.x = math.random()*resx
+	new.y = math.random()*resy
+	new.w = 10+math.random()*20
+	return new
+end
+
+function Cloud:update(dt)
+	self.x = self.x+dt
+end
+
+function Cloud:draw()
+	love.graphics.ellipse("fill", self.x, self.y, self.w*love.math.noise(self.x/50,self.y/50))
 end
