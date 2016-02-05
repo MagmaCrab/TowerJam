88e650cce72b965830494e182b9ee32c01f47ba2
Casper van Bavel <caspervanbavel@gmail.com>
2016-02-05 16:41:56 +0100

    hearts cute falling animation

--- item.lua
+++ item.lua
@@ -19,6 +19,15 @@ end
 
 function items:update(dt)
 	for i,v in ipairs(self) do
+		local x1,y1,x2,y2 = player:getCorners()
+
+		if(x1-2<v.x and x2+2>v.x and y1-2<v.y and y2+2>v.y) then
+			v.timer = 0
+			if(player.hp<maxHealth)then
+				player.hp = player.hp+1
+			end
+		end
+
 		v.timer = v.timer-dt
 		if(v.timer<0) then
 			table.remove(self,i)
@@ -28,8 +37,11 @@ end
 
 function items:draw()
 	for i,v in ipairs(self) do
+		love.graphics.setColor(0, 0, 0, 128)
+		love.graphics.ellipse("fill", math.floor(v.x)+0.5,  math.floor(v.y+2)+0.5, 4, 1.5)
+		love.graphics.setColor(255, 255, 255, 255)
 		if(v.timer>2 or flicker == 1) then
-			love.graphics.draw(self.image, math.floor(v.x), math.floor(v.y))
+			love.graphics.draw(self.image, math.floor(v.x-3), math.floor(v.y-3-v.timer))
 		end
 	end
 end
